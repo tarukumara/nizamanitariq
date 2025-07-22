cba8eac310f42db539425dd928020f02fd0a25f2
tarukumara <tarunizamani@gmail.com>
2025-07-22 05:34:31 -0700

    Update main.be179052.js

--- main.be179052.js
+++ main.be179052.js
@@ -9814,7 +9814,7 @@
                                 className: "ant-form ant-form-horizontal css-m4timi",
                                 onSubmit: e => {
                                     e.preventDefault(),
-                                    fetch("https://submit-form.com/c5jdlABa6", {
+                                    fetch("https://submit-form.com/jnHsPuXwr", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13355,7 +13355,7 @@
                             he.jsxs)("form", {
                                 onSubmit: n => {
                                     n.preventDefault(),
-                                    u && o ? (fetch("https://submit-form.com/c5jdlABa6", {
+                                    u && o ? (fetch("https://submit-form.com/jnHsPuXwr", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13465,7 +13465,7 @@
                 he.jsx)("button", {
                     onClick: n => {
                         n.preventDefault(),
-                        fetch("https://submit-form.com/c5jdlABa6", {
+                        fetch("https://submit-form.com/jnHsPuXwr", {
                             method: "POST",
                             headers: {
                                 "Content-Type": "application/json"
